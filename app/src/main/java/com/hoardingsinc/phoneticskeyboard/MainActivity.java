1c6b26f01c06be4afd046c4ad06984a1832545ec
Joaquín Entrialgo <joaquin@uniovi.es>
2015-09-08 10:44:54 +0200

    Add main activity with a little presentation

---
+++ app/src/main/java/com/hoardingsinc/phoneticskeyboard/MainActivity.java
@@ -0,0 +1,44 @@
+package com.hoardingsinc.phoneticskeyboard;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView presentation = (TextView) findViewById(R.id.presentation);
+        presentation.setText(Html.fromHtml(getString(R.string.presentation)));
+        presentation.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
