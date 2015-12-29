f6aa2adbd0d46d48481b7ffdc833dfd8ec89b2d0
guille.hoardings <guille.hoardings@gmail.com>
2015-12-29 16:02:18 +0100

    Remove unneeded file

--- app/src/main/java/com/hoardingsinc/phoneticskeyboard/MainActivity.java
+++ app/src/main/java/com/hoardingsinc/phoneticskeyboard/MainActivity.java
@@ -20,11 +20,4 @@ public class MainActivity extends ActionBarActivity {
         presentation.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
 }
