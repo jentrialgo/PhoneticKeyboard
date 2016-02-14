6c25a021681adb09d4d2c3fa36fb9161c02c0882
Joaquín Entrialgo <joaquin@uniovi.es>
2016-02-14 10:33:40 +0100

    Complete the Pronunroid ad

--- app/src/main/java/com/hoardingsinc/phoneticskeyboard/MainActivity.java
+++ app/src/main/java/com/hoardingsinc/phoneticskeyboard/MainActivity.java
@@ -1,15 +1,18 @@
 package com.hoardingsinc.phoneticskeyboard;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,10 @@ public class MainActivity extends ActionBarActivity {
         TextView presentation = (TextView) findViewById(R.id.presentation);
         presentation.setText(Html.fromHtml(getString(R.string.presentation)));
         presentation.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView pronunroidAdd = (TextView) findViewById(R.id.pronunroid_ad);
+        pronunroidAdd.setText(Html.fromHtml(getString(R.string.pronunroid_ad)));
+        pronunroidAdd.setMovementMethod(LinkMovementMethod.getInstance());
 
         KeyboardPreferences keyboardPreferences = new KeyboardPreferences(this);
 
@@ -31,6 +38,9 @@ public class MainActivity extends ActionBarActivity {
                 radioGroup.check(R.id.radio_extended_layout);
                 break;
         }
+
+        Button b = (Button) findViewById(R.id.button_pronunroid_ad);
+        b.setOnClickListener(this);
     }
 
     public void onRadioButtonClicked(View view) {
@@ -49,5 +59,12 @@ public class MainActivity extends ActionBarActivity {
                 keyboardPreferences.saveLayout(KeyboardPreferences.LAYOUT_EXTENDED);
                 break;
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        // There's only one button: Pronunroid ad
+        startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse(
+                "https://play.google.com/store/apps/details?id=com.hoardingsinc.pronunroid")));
     }
 }
